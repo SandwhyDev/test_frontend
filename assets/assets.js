c0b995654273d099e3cb29adc403d959eec190a5
SandwhyDev <sandwhydev@gmail.com>
2022-08-26 17:06:41 +0700

    up

--- assets/assets.js
+++ assets/assets.js
@@ -1,5 +1,6 @@
 import activity1 from "./activity-empty-state.svg";
 import Todo from "./todo-empty-state.svg";
 import Sukses from "./Sukses.svg";
+import Alert from "./Alert.svg";
 
-export { activity1, Todo, Sukses };
+export { activity1, Todo, Sukses, Alert };
--- components/CardTodo.jsx
+++ components/CardTodo.jsx
@@ -1,16 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { HiOutlineTrash } from "react-icons/hi";
+import { Alert, Sukses } from "../assets/assets";
 import Modal from "./Modal";
+import Image from "next/image";
 
 const CardTodo = ({ title = "Title", date = "5 oktober 2021", id }) => {
   const [modal, setModal] = useState(false);
 
-  const handleDelete = (id) => {
-    let yakin = confirm(`yakin hapus ${id} ?`);
-    if (!yakin) {
-      return false;
-    }
+  const handleDelete = () => {
+    setModal(!modal);
+  };
+
+  const showDelete = (id) => {
     axios(
       `https://floating-mountain-35184.herokuapp.com/activity-groups/${id}`,
       {
@@ -23,7 +25,6 @@ const CardTodo = ({ title = "Title", date = "5 oktober 2021", id }) => {
     )
       .then((res) => {
         if (res.status === 200) {
-          setModal(!modal);
           window.location.reload();
         }
       })
@@ -33,7 +34,36 @@ const CardTodo = ({ title = "Title", date = "5 oktober 2021", id }) => {
   };
   return (
     <div className="w-[150px] h-[150px] rounded-xl bg-white shadow-[0_6px_10px_0px_rgba(0,0,0,0.1)] p-3 flex flex-col justify-between">
-      {modal && <Modal title="Activity berhasil dihapus" />}
+      {modal && (
+        <div className="w-screen h-screen flex flex-col items-center justify-center bg-black/50 fixed top-0 left-0 px-5 z-50">
+          <div className="w-full  rounded-xl text-sm  bg-white shadow-[0_4px_10px_0px_rgba(0,0,0,0.1)] flex flex-col items-center justify-center gap-7 p-4 text-center ">
+            <Image src={Alert} priority={true} />
+            <h1>
+              Apakah anda yakin menghapus activity{" "}
+              <span className="font-bold">"{id}"?</span>
+            </h1>
+
+            <div className="flex gap-4 w-full">
+              <button
+                onClick={() => {
+                  window.location.reload();
+                }}
+                className="bg-[#F4F4F4] p-3 w-full rounded-full font-bold text-[#4A4A4A]"
+              >
+                Batal
+              </button>
+              <button
+                className="bg-[#ED4C5C] p-3 w-full rounded-full text-white font-bold"
+                onClick={() => {
+                  showDelete(id);
+                }}
+              >
+                Hapus
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
 
       <h1 className="font-bold capitalize">{title}</h1>
 
