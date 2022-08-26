714139f3d903ba647b48c1a89654389d8a10887d
SandwhyDev <sandwhydev@gmail.com>
2022-08-26 17:20:03 +0700

    asdasd

--- assets/assets.js
+++ assets/assets.js
@@ -1,6 +1,6 @@
 import Activity1 from "./activity-empty-state.svg";
 import Todo from "./todo-empty-state.svg";
 import Sukses from "./Sukses.svg";
-import Alert from "./Alert.svg";
+import Alert from "./alert.svg";
 
 export { Activity1, Todo, Sukses, Alert };
